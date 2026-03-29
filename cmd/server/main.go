1e56124cfd38bf0d00930e4cc1e9b624985fb85c
F3dosik <izhevsk2073@gmail.com>
2026-03-29 14:25:22 +0400

    feat: add build info output on startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"cmp"
+	"fmt"
 	"log"
 
 	cfg "github.com/F3dosik/metalert/internal/config/server"
@@ -8,7 +10,16 @@ import (
 	"github.com/F3dosik/metalert/pkg/logger"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
 func main() {
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n",
+		cmp.Or(buildVersion, "N/A"),
+		cmp.Or(buildDate, "N/A"),
+		cmp.Or(buildCommit, "N/A"))
+
 	cfg, err := cfg.LoadServerConfig()
 	if err != nil {
 		log.Fatalf("Configuration loading error: %v", err)
